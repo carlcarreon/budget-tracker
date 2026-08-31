2e7b13499f848c63aceeea730d4ba0001c025d85
Carl Justin Carreon <carreon.carll@gmail.com>
2026-08-31 15:25:26 +0800

    fix useState issue on home

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { useRegisterSW } from 'virtual:pwa-register/react'
+import { registerSW } from 'virtual:pwa-register'
 
-useRegisterSW({ immediate: true })
+registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
