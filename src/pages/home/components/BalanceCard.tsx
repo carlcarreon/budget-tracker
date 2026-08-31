4b4b3b6e14f6b3bd82a2a598dacefb6b8da79f2b
Carl Justin Carreon <carreon.carll@gmail.com>
2026-08-31 23:09:02 +0800

    modify balance card

--- src/pages/home/components/BalanceCard.tsx
+++ src/pages/home/components/BalanceCard.tsx
@@ -1,8 +1,8 @@
-type BalanceCardProps = {
-  totalBalance: number
-  orderCount: number
-  savings: number
-  paylaterCount: number
-  formatCurrency: (value: number) => string
-  formatSignedCurrency: (value: number) => string
-}
+// type BalanceCardProps = {
+//   totalBalance: number
+//   orderCount: number
+//   savings: number
+//   paylaterCount: number
+//   formatCurrency: (value: number) => string
+//   formatSignedCurrency: (value: number) => string
+// }
